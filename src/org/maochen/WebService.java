7481041ec23509071c5cbcd4f5ae11a1446f203b
maochen <contact@maochen.org>
2014-02-05 17:14:53 -0500

    jersey module added

---
+++ src/org/maochen/WebService.java
@@ -0,0 +1,31 @@
+package org.maochen;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+@Produces(MediaType.TEXT_HTML)
+public class WebService {
+
+    // @Path("/{order}")
+    // @PUT
+    // @Produces("text/html")
+    // public String create(@PathParam("order") String order, @QueryParam("customer_name") String
+    // customerName) {
+    //
+    // return "Added order #" + order;
+    // }
+
+    @Path("/ws")
+    @GET
+    @Produces("text/html")
+    public String find(@PathParam("name") final String name) {
+        String returnStr = "Jersey REST WebSerivce Started";
+        if (name != null && !name.trim().isEmpty()) returnStr += " for" + name;
+        return returnStr;
+    }
+
+}

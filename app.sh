8252f09e708c091a099f96366a029725b512bd56
maochen <contact@maochen.org>
2014-03-10 00:52:35 -0400

    Change to retina version pic resolution

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
-APPCFG=/Applications/eclipse/plugins/com.google.appengine.eclipse.sdkbundle_1.9.0/appengine-java-sdk-1.9.0/bin/appcfg.sh
-#APPCFG=~/springsource/sts-3.4.0.RELEASE/plugins/com.google.appengine.eclipse.sdkbundle_1.8.9/appengine-java-sdk-1.8.9/bin/appcfg.sh
+#APPCFG=/Applications/eclipse/plugins/com.google.appengine.eclipse.sdkbundle_1.9.0/appengine-java-sdk-1.9.0/bin/appcfg.sh
+APPCFG=~/springsource/sts-3.4.0.RELEASE/plugins/com.google.appengine.eclipse.sdkbundle_1.9.0/appengine-java-sdk-1.9.0/bin/appcfg.sh
 
 case "$1" in
 	server)

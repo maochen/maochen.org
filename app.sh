5065e8086d0237338f258417a0ce8d3f4da4575a
Maochen G <contact@maochen.org>
2015-06-11 00:59:04 -0400

    change app.sh

--- app.sh
+++ app.sh
@@ -5,6 +5,7 @@ case "$1" in
 		mvn clean install appengine:devserver
 		;;
 	upload)
+		mvn clean install -U
 		sudo chmod +x $APPCFG/appcfg.sh
 		sudo chmod +x $APPCFG/run_java.sh
 		sudo $APPCFG/appcfg.sh update war	

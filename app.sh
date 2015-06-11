e8817084a82ecd0ea18fb064cd09924c70031c9c
Maochen G <contact@maochen.org>
2015-06-10 23:26:57 -0400

    upgrade gae to 1.9.22

--- app.sh
+++ app.sh
@@ -1,4 +1,4 @@
-APPCFG=~/.m2/repository/com/google/appengine/appengine-java-sdk/1.9.17a/appengine-java-sdk/appengine-java-sdk-1.9.17/bin
+APPCFG=~/.m2/repository/com/google/appengine/appengine-java-sdk/1.9.22/appengine-java-sdk/appengine-java-sdk-1.9.22/bin
 
 case "$1" in
 	server)

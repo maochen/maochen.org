ec12d82f18eb922074ad52741e415676a2af06df
Maochen G <contact@maochen.org>
2016-12-29 21:28:53 -0500

    update gae to 1.9.48

--- app.sh
+++ app.sh
@@ -1,18 +1,20 @@
-GAE_VERSION=1.9.44
+#!/usr/bin/env bash
+
+GAE_VERSION=1.9.48
 APPCFG=~/.m2/repository/com/google/appengine/appengine-java-sdk/$GAE_VERSION/appengine-java-sdk/appengine-java-sdk-$GAE_VERSION/bin
 
 case "$1" in
-	server)
-		mvn clean install appengine:devserver
-		;;
-	upload)
-		mvn clean install -U
-		sudo chmod +x $APPCFG/appcfg.sh
-		sudo chmod +x $APPCFG/run_java.sh
-		sudo $APPCFG/appcfg.sh update war	
-		;;
-	rollback)
-		$APPCFG/appcfg.sh rollback war
-		;;
-	*)
+  server)
+    mvn clean install appengine:devserver
+    ;;
+    upload)
+        mvn clean install -U
+        sudo chmod +x $APPCFG/appcfg.sh
+        sudo chmod +x $APPCFG/run_java.sh
+        sudo $APPCFG/appcfg.sh update war
+        ;;
+    rollback)
+        $APPCFG/appcfg.sh rollback war
+        ;;
+    *)
 esac

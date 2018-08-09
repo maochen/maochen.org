07515acb710e1b3872d6d05bb703645de9dd54ee
Maochen G <contact@maochen.org>
2018-08-08 21:37:14 -0700

    update

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-GAE_VERSION=1.9.63
+GAE_VERSION=1.9.64
 APPCFG=~/.m2/repository/com/google/appengine/appengine-java-sdk/$GAE_VERSION/appengine-java-sdk/appengine-java-sdk-$GAE_VERSION/bin
 
 case "$1" in
